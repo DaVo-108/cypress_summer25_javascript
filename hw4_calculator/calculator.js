edea3ce7afeb4d04c51bef62595643e22d79e427
Daniela Vondrová <daniela.jurasikova108@gmail.com>
2025-08-09 19:10:10 +0200

    hw5 Calculator oop

--- hw4_calculator/calculator.js
+++ hw4_calculator/calculator.js
@@ -1,1 +1,19 @@
-const number1 = 565 
+const a = 565;
+const b = 584;
+result = a + b;
+console.log("Součet: " + result);
+
+const c = 474;
+const d = 52;
+result = c - d;
+console.log("Odečet: " + result);
+
+const e = 5822;
+const f = 65;
+result = e * f;
+console.log("Násobek: " + result);
+
+const g = 632;
+const h = 6;
+result = g / h;
+console.log("Dělení: " + result);
